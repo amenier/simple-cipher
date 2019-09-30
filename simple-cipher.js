059fdde96111bf5b4554b30ddeaeda4f1a474ca0
amenier <amanda.menier@gmail.com>
2019-09-29 23:15:38 -0400

    Cleaned a bit and added comments. Negative edge case needs work

--- simple-cipher.js
+++ simple-cipher.js
@@ -3,10 +3,12 @@
 //declare alphabet. Alphabet can be changed to further customize the encoding of the message.
 let alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
 
+/*encode funciton takes a string to encode and a number to shift the letters by
+the number can be positive or neagtive, an integer or float. the shift will be 
+rounded down to the nearest integer */
 function encode(message, shift) {
 
-    console.log(typeof message);
-
+    //first section checks for certain edge cases and stops function if necessary
     if ((typeof message) !== 'string') {
         return console.log("Must include a valid message in first argument");
     }
@@ -18,18 +20,28 @@ function encode(message, shift) {
         return console.log("That isn't a very good cipher, try again");
     }
 
+    //sets the message to be lower case to ensure everything is processed
+    //I may work on making sure case is matched in the final message later
     message = message.toLowerCase();
-    console.log(message);
-    
+
+    /*this section makes sure that the shift number can be properly used in the below section
+    if the number is greater than 26, the modulo remainder will be used to determine the shift
+    then the number is rounded down in case it is a float
+    finally, if the last number is negative, then a postive shift is calculatd*/
     if (shift > 26) shift = shift%26;
     console.log(shift);
     shift = Math.floor(shift);
     console.log(shift);
     if (shift < 0) shift = 26 + shift;
+    //I need to do the math to make sure this still works if a large neagtive number is added
+    // maybe divide everything by 26, then if its negative subtract the remained from 26 to get the shift?
 
-    
+    //creates an empty string for the encoded message
     let encodedMessage = '';
     
+    /*runs through each character of the message. If the character isn't found, it copies
+    it into the new string. does a forward shift or backward shift based on
+    whether the shift would hit the end of the index*/
     for (letter of message) {
         originalIndex = alphabet.indexOf(letter);
         if (originalIndex === -1) {
@@ -47,4 +59,5 @@ encode(NaN, 7);
 encode("hello", true);
 encode('salads', 0);
 encode("message",-5);
+encode("negative", -500.42);
 encode("I love you", 1000);
